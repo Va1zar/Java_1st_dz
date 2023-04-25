a7cd59fac4c481fec71d7ae3497a2d3c1d177344
Evgeniy <s.h.a.r.k.112@mail.ru>
2023-04-25 17:15:56 +0700

    solve of task_1

--- third_dz/task_1.java
+++ third_dz/task_1.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
 
 public class task_1 {
     public static void main(String[] args) {
-        Random random = new Random(); // рандомное заполнение массива
+        Random random = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите кол-во элементов массива : ");
         int arr_size = sc.nextInt();
         int[] arr = new int[arr_size];
         System.out.println("\n Первоначальный массив : ");
         for (int i = 0; i < arr_size; i++) {
-            arr[i] = random.nextInt(2, 21);
+            arr[i] = random.nextInt(2, 21); // рандомное заполнение массива
         }
         for (int i = 0; i < arr_size; i++) {
-            System.out.print(" " + arr[i]);
+            System.out.print(" " + arr[i]); // выводим его на печать
         }
-        System.out.println("\n Отсортированный массив : ");
+        System.out.println("\n Отсортированный массив только с нечетными числами : ");
         for (int i = 0; i < arr_size; i++) {
-            if (arr[i] % 2 == 1) {
+            if (arr[i] % 2 == 1) { // проверка на четность
                 System.out.printf(" " + arr[i]);
             }
         }
---
+++ third_dz/task_2.java
@@ -0,0 +1,9 @@
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка. Collections.max()
+
+package third_dz;
+
+public class task_2 {
+    public static void main(String[] args) {
+
+    }
+}
