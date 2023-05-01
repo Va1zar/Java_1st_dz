4bec1cdd72c62c6e8be626c46ffe257dbb832dda
Evgeniy <s.h.a.r.k.112@mail.ru>
2023-05-01 16:56:09 +0700

    solve of task_3

---
+++ forth_dz/task_3.java
@@ -0,0 +1,25 @@
+// Найдите сумму всех элементов LinkedList
+// сохраняя все элементы в списке. Используйте итератор
+
+package forth_dz;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class task_3 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(0, i);
+        }
+        System.out.println(list);
+        Iterator<Integer> lisIterator = list.iterator();
+        int sum = 0;
+        while (lisIterator.hasNext()) {
+            sum += lisIterator.next();
+        }
+        System.out.printf("Сумма всех элементов  = %d", sum);
+    }
+}
